83bd260c0e9b4c04a1889044a2f69418a9db4e88
Methilan-R <rmethilan@gmail.com>
2025-08-16 14:59:49 +0530

    render

--- server/index.js
+++ server/index.js
@@ -5,13 +5,8 @@ import dotenv from 'dotenv'
 import clientRoutes from './routes/client.js'
 dotenv.config()
 const app = express()
- 
-app.use(cors({
-  origin: ['http://localhost:3000','https://onboard-m867.vercel.app'], // or '*' for all origins
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+const port = process.env.PORT || 4000
+app.use(cors());
 
 
  
@@ -24,7 +19,7 @@ app.get('/',(req,res)=>{
     return res.send("Backend is Running")
 })
 
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log("Server Running on 4000")
 })
 
